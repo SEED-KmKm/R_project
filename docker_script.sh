5b74c9c2669d69805f886474f19a81e1b5df41aa
SEED-KmKm <megane415583@gmail.com>
2024-07-12 18:34:51 +0900

    added USERSEtting

--- docker_script.sh
+++ docker_script.sh
@@ -8,4 +8,11 @@ PATH_sz5 = '/home/seed/Desktop/Projects/R_project:/home/rstudio rocker/verse'
 docker run --rm -d -p 8787:8787  -e PASSWORD=oyakokatsu -v /home/seed/Desktop/Projects/R_project:/home/rstudio rocker/tidyverse
 
 #---sz5
-docker run --rm -d -p 8787:8787  -e PASSWORD=oyakokatsu -v /home/seed/Desktop/projects/R_project:/home/rstudio rocker/verse
+docker run -it --rm -d -p 8787:8787  -e PASSWORD=oyakokatsu -e USERID=1000  -v /home/seed/Desktop/projects/R_project:/home/rstudio rocker/verse
+
+# docker run -it --rm -d -p 8787:8787  -e PASSWORD=oyakokatsu -e USERID=$(id -u $USER)  -v /home/seed/Desktop/projects/R_project:/home/rstudio rocker/verse
+# sudo docker run -it --rm -d -p 8787:8787  -e PASSWORD=oyakokatsu -e USERID=$(id -u $USER) -u "$(id -u $USER):$(id -g $USER)" -v /home/seed/Desktop/projects/R_project:/home/rstudio rocker/verse
+# sudo docker run -it --rm -d -p 8787:8787  -e PASSWORD=oyakokatsu  -u 1000:1000 -v /home/seed/Desktop/projects/R_project:/home/rstudio rocker/verse
+# sudo docker run -it --rm -d -p 8787:8787  -e PASSWORD=oyakokatsu   -v /home/seed/Desktop/projects/R_project:/home/rstudio rocker/verse
+
+# sudo docker run -it --rm -d -p 8787:8787  -e PASSWORD=oyakokatsu -v /home/seed/Desktop/projects/R_project:/home/rstudio rocker/verse
